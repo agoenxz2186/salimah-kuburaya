1e36b6a75e291c958571a8bc1607595440098d5b
Agung Sasongko <89963730+agoenxz2186@users.noreply.github.com>
2024-11-08 13:55:47 +0700

    Update and rename galeri.php to Galeri.php

--- app/Models/Galeri.php
+++ app/Models/Galeri.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class galeri extends Model
+class Galeri extends Model
 {
     use HasFactory;
     protected $fillable = ['judul', 'youtube_code'];
